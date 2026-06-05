fb3621f4d6c14e6bdc6d4b9f649f6d2d03da7f2f
aycman <man.hajilou@gmail.com>
2026-06-05 13:05:18 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/03_e-shop-watches_redux-tailwindcss/',
+  base: '/e-shop-watches/',
   plugins: [react()],
 })
