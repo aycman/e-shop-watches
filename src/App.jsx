b61281c972fdfbe43b10ad802b23e3550ebf1427
aycman <man.hajilou@gmail.com>
2026-06-05 12:53:46 +0200

    deploy with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="bg-[#0a0a0a] min-h-screen font-sans selection:bg-[#d4af37] selection:text-black flex flex-col">
         <Navbar />
         {/* Routing Setup */}
@@ -21,7 +21,7 @@ function App() {
         <Footer />
       </div>
       
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
